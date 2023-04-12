0d22b6741a03d7f40e1204934cbe4caf0e9c846a
David Reidsma <david.reidsma@gmail.com>
2023-04-12 17:37:27 -0400

    hooked up to calvin iot server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IMqttMessage, MqttModule, IMqttServiceOptions, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
+
+export interface MotorState {
+  left: number;
+  right: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rcx-controller';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'RCX Controller';
+  currentSpeed = 2;
+  
+  private subscription: Subscription;
+  private topic = 'cs326rcxcontroller';
+
+  constructor(private mqttService: MqttService) {
+    this.subscription = mqttService
+      .observe(this.topic)
+      .subscribe((message: IMqttMessage) => {
+        console.log(message.payload.toString());
+      });
+  }
+
+  remoteButtonPressed(button: string) {
+    const halfSpeed = Math.floor(this.currentSpeed / 2);
+
+    let right: number = 0;
+    let left: number = 0;
+
+    switch(button) {
+      case 'fwdLeft':
+        right = this.currentSpeed;
+        left = halfSpeed;
+        break;
+      case 'fwd':
+        left = this.currentSpeed;
+        right = this.currentSpeed;
+        break;
+      case 'fwdRight':
+        left = this.currentSpeed;
+        right = halfSpeed;
+        break;
+      
+      case 'left':
+        left = -this.currentSpeed;
+        right = this.currentSpeed;
+        break;
+      case 'stop':
+        break;
+      case 'right':
+        left = this.currentSpeed;
+        right = -this.currentSpeed;
+        break;
+      
+      case 'bwdLeft':
+        right = -this.currentSpeed;
+        left = -halfSpeed;
+        break;
+      case 'bwd':
+        left = -this.currentSpeed;
+        right = -this.currentSpeed;
+        break;
+      case 'bwdRight':
+        left = -this.currentSpeed;
+        right = -halfSpeed;
+        break;
+      default:
+        break;
+    }
+
+    const motorState: MotorState = {
+      left,
+      right
+    };
+
+    this.mqttService.unsafePublish(this.topic, JSON.stringify(motorState));
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment as env } from '../environments/environment.prod'
+import {
+  MqttModule,
+  IMqttServiceOptions,
+} from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: env.mqtt.hostname,
+  port: env.mqtt.port,
+  path: env.mqtt.path,
+  protocol: env.mqtt.protocol === 'wss' ? 'wss' : 'ws',
+  username: env.mqtt.username,
+  password: env.mqtt.password
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
